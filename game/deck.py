e5d2843cfc17656a5c736471a1a8305e02d8daef
Gleison Batista <gleisonbs@gmail.com>
2024-01-18 23:13:11 -0300

    refactor: move deck shuffle outside of constructor

--- game/deck.py
+++ game/deck.py
@@ -8,7 +8,9 @@ class Deck:
     def __init__(self):
         self.deck = [f'{r}{s}' for s in Deck.suits
                                for r in Deck.ranks]
-        random.shuffle(self.deck)
 
     def draw_cards(self, quantity):
         return [self.deck.pop() for _ in range(quantity)]
+
+    def shuffle(self):
+        random.shuffle(self.deck)
